6a5e44dac9c8a884d771e97367072fa5fbf5a43a
Isaac Jaffe <isaacjaffe@me.com>
2021-01-15 16:06:39 -0500

    fixed models removed warnings

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -13,7 +13,7 @@ class Todo(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
     completed_time = models.DateTimeField(null=True, blank=True)
-    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES, default='med')
+    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES, default='med', null=True, blank=True)
     due_date = models.DateTimeField(null=True, blank=True)
 
     def _str_(self):
