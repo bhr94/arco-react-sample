ad8c246410828c3615512d16347c35272822b889
Isaac Jaffe <isaacjaffe@me.com>
2021-01-15 15:51:50 -0500

    model change for bugfix

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -13,7 +13,7 @@ class Todo(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
     completed_time = models.DateTimeField(null=True, blank=True)
-    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES)
+    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES, default='med')
     due_date = models.DateTimeField(null=True, blank=True)
 
     def _str_(self):
