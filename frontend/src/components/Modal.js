17201fe344e48f5a765394f2c7642f4b2a9aed9b
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-21 10:28:50 -0800

    A - added a duedate form into the modal

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -21,6 +21,12 @@ export default class CustomModal extends Component {
       activeItem: this.props.activeItem,
     };
   }
+  // componentDidUpdate() {
+  //   // Access ISO String and formatted values from the DOM.
+  //   var hiddenInputElement = document.getElementById("example-datepicker");
+  //   console.log(hiddenInputElement.value); // ISO String, ex: "2016-11-19T12:00:00.000Z"
+  //   console.log(hiddenInputElement.getAttribute("data-formattedvalue")); // Formatted String, ex: "11/19/2016"
+  // }
   handleChange = (e) => {
     let { name, value } = e.target;
     if (e.target.type === "checkbox") {
@@ -79,12 +85,11 @@ export default class CustomModal extends Component {
             </FormGroup>
             <FormGroup>
               <Label for="duedate">Due Date</Label>
-              <Input
-                type="text"
+              <DatePicker
                 name="duedate"
-                value={this.state.activeItem.duedate}
-                onChange={this.handleChange}
+                value={this.state.duedate}
                 placeholder="Enter Todo duedate"
+                onChange={this.handleChange}
               />
             </FormGroup>
           </Form>
